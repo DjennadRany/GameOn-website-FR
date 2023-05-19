f34044f468c01055da51c3884f1c11a026e4fc52
DjennadRany <128905298+DjennadRany@users.noreply.github.com>
2023-05-19 10:45:35 +0200

    Add files via upload

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// Fonction pour ajouter ou supprimer la classe "responsive" sur la navigation
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,128 +8,84 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Récupération des éléments du DOM
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const close = document.getElementById("close");
-const btnVal = document.querySelectorAll("#val");
-
+const modalBtns = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelector("#close");
+const ferme = document.querySelector("#ferme");
+const submitBtn = document.querySelector("#val");
 const form = document.getElementById("val");
-const log = document.getElementById("log");
+const modalBody = document.querySelector('#modal-body');
+const modalVal = document.querySelector("#mod-val");
 
-const modbod = document.getElementsByClassName('modal-body');
+// Écouteur d'événement pour soumettre le formulaire
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
 
-
-const modval = document.querySelectorAll(".mod-val");
-const btnSub = document.querySelectorAll("#subm");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-  console.log('lauchn')
-}
-// close modal
-close.addEventListener('click', function myFunction() {
-  modalbg.style.display = "none";
-});
-
-
-
-
-
-// vérification input et submit  modal
-function vali(evt) {
-evt.preventDefault()
-
- 
-  console.log(evt);
-  var name = document.forms["myForm"]["name"];
-  var last = document.forms["myForm"]["last"];
-  var email = document.forms["myForm"]["email"];
-  var birthdate = document.forms["myForm"]["birthdate"];
-  var quantity = document.forms["myForm"]["quantity"];
-  var checkbox1 = document.forms["myForm"]["checkbox1"];
-
-
-
-
-
-
-
-  if (document.getElementById("name").value == "") {
-    document.getElementById('errorname').innerHTML = "Veuillez entrez un prénom valide";
-    name.focus();
-    return false;
-  } else {
-    document.getElementById('errorname').innerHTML = "";
-  }
-
-  if (document.getElementById("last").value == "") {
-    document.getElementById('errorlast').innerHTML = "Veuillez entrez un nom valide";
-    last.focus();
-    return false;
-  } else {
-    document.getElementById('errorname').innerHTML = "";
-  }
-  if (document.getElementById("email").value == "") {
-    document.getElementById('erroremail').innerHTML = "Veuillez entrez une adresse mail valide";
-    email.focus();
-    return false;
-  } else {
-    document.getElementById('erroremail').innerHTML = "";
-  }
-  
-  if (document.getElementById("birthdate").value == "") {
-    document.getElementById('errorbirthdate').innerHTML = "Veuillez entrez une date de naissance valide";
-    birthdate.focus();
-    return false;
-  } else {
-    document.getElementById('errorbirthdate').innerHTML = "";
-  }
-  if (document.getElementById("quantity").value == "") {
-    document.getElementById('errorequantity').innerHTML = "Veuillez entrez un nombre de tournois valide";
-    quantity.focus();
-    return false;
-  } else {
-    document.getElementById('errorequantity').innerHTML = "";
-  }
-  
-  var loca = document.forms["myForm"]["location"];
-  for (var i=0;  loca[i]; ++i) {
-    console.log("resulta " + loca.value)
-    if (!loca.value == Boolean(false) ) {
-  console.log("elle passe ")
-      document.getElementById('errorlocal').innerHTML = "";
+  function checkbox(input, error, errorLabel) {
+    if (!input.checked) {
+      error.textContent = errorLabel;
+      input.focus();
+      return false;
     } else {
-      console.log("elle ne passe pas ")
-      document.getElementById('errorlocal').innerHTML = "Veuillez cochez une localité";
-      loca.focus();
+      error.textContent = "";
+      return true;
     }
   }
-  if (checkbox1.checked === false) {
-    document.getElementById('errorchek').innerHTML = "Veuillez cochez l'RGPD";
-    checkbox1.focus();
-    console.log("hello bugge2")
-    return false;
-  } else {
-    document.getElementById('errorchek').innerHTML = "";
+
+  function input(input, error, errorLabel) {
+    if (input.value.trim() === "") {
+      error.textContent = errorLabel;
+   
+      return false;
+    } else {
+      error.textContent = "";
+      return true;
+    }
   }
 
+  const checkboxError = document.querySelector('#errorchek');
+  const nameError = document.querySelector('#errorname');
+  const lastNameError = document.querySelector('#errorlast');
+  const emailError = document.querySelector('#erroremail');
+  const birthdateError = document.querySelector('#errorbirthdate');
+  const locationError = document.querySelector('#errorlocal');
+  const quantityError = document.querySelector('#errorequantity');
 
+  const checkboxInput = document.querySelector('#checkbox1');
+  const nameInput = document.querySelector('#name');
+  const lastNameInput = document.querySelector('#last');
+  const emailInput = document.querySelector('#email');
+  const birthdateInput = document.querySelector('#birthdate');
+  const locationInput = document.forms["myForm"]["location"];
+  const quantityInput = document.querySelector('#quantity');
 
-  console.log("hello world")
+  const isCheckboxValid = checkbox(checkboxInput, checkboxError, "Veuillez entrer un nombre de tournois valide");
+  const isNameValid = input(nameInput, nameError, "Veuillez entrer un nom valide");
+  const isLastNameValid = input(lastNameInput, lastNameError, "Veuillez entrer un nom valide");
+  const isEmailValid = input(emailInput, emailError, "Veuillez entrer une adresse e-mail valide");
+  const isBirthdateValid = input(birthdateInput, birthdateError, "Veuillez entrer une date de naissance valide");
+  const isLocationValid = input(locationInput, locationError, "Veuillez entrer une localité");
+  const isQuantityValid = input(quantityInput, quantityError, "Veuillez entrer un nombre de tournois valide");
 
-  modalbg.style.display = "block";
-  document.getElementById("modal-body").style.display = "none";
-  document.getElementById("mod-val").style.display = "block";
-  
+  if (isCheckboxValid && isNameValid && isLastNameValid && isEmailValid && isBirthdateValid && isLocationValid && isQuantityValid) {
+    
+    form.reset(); // Réinitialise le formulaire après l'envoi
+    modalBody.style.display = "none";
+    modalVal.style.display = "block";
+  } 
+});
 
+// Écouteurs d'événement pour ouvrir et fermer la modale
+modalBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    modalbg.style.display = "block";
+  });
+});
 
-}
-
-form.addEventListener("submit", vali);
-
+closeBtn.addEventListener('click', () => {
+  modalbg.style.display = "none";
+});
+ferme.addEventListener('click', () => {
+  modalbg.style.display = "none";
+});
